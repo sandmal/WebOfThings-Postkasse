82e137ad50afdcc675335e50b93f4ddab7afdc20
Sandmal <sandmal.cornelius@gmail.com>
2021-11-09 14:15:33 +0100

    Something that is working

---
+++ index.js
@@ -0,0 +1,31 @@
+var mqtt = require('mqtt');
+var client = mqtt.connect('mqtt://broker.hivemq.com');
+
+const topic = 'SmartMailBoxNTNU/wot';
+
+client.on('connect', () => {
+  console.log('Connected');
+  client.subscribe([topic], () => {
+    console.log(`Subscribe to topic '${topic}'`);
+  });
+});
+
+client.on('message', function (topic, message) {
+  // message is Buffer
+ //console.log(topic + ': ' + message.toString());
+  if(message.toString() == 'Mail received'){
+    console.log('Mail received');
+  }
+
+  if (message.toString() == 'Mailbox is full') {
+    console.log('Mailbox is full');
+  }
+
+  if(message.toString() == 'newTopic') {
+    client.subscribe([topic], () => {
+      console.log(`New subscriber to '${topic}'`);
+    })
+  }
+});
+
+
